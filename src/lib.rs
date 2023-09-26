df6590441811eb4f921a6de478bf6c5d6e13cdd4
kebtech <64436886+kebtech@users.noreply.github.com>
2023-09-26 10:23:00 -0400

    added get_relative_path

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::path::Path;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn get_relative_path(parent: &Path, child: &Path) -> Option<String> {
+    child.strip_prefix(parent).ok().map(|path| path.to_str().unwrap().to_string())
 }
